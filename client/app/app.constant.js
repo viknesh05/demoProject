d4a229fc747f691d932533d1467113dbe7e39da1
Ubuntu <ubuntu@ip-172-31-1-205.us-west-1.compute.internal>
2016-01-07 17:56:50 +0000

    Initial commit

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('demoProjectApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('demoProjectApp', [
+  'demoProjectApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
